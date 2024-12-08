051bdc0cb42e1277af61c3000230a88a6f8edd23
Infinity <kingsleyoforiatta70@gmail.com>
2024-12-08 15:36:01 +0000

    Version 1

---
+++ Weatheroption/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the temperature Converter!");
+        System.out.println("1. Convert Celsius to Fehrenheit!");
+        System.out.println("2. Convert Fehrenheit to Celsius!");
+    }
+}
